02522434e18d11add381e5c0c1071d139bb44b75
hindrekhan <hindrek.hannus@tptlive.ee>
2018-01-09 11:03:25 +0200

    fixed road generator

--- Budget game/Budget game/Terrain.cs
+++ Budget game/Budget game/Terrain.cs
@@ -20,30 +20,75 @@ namespace Budget_game
             Random rnd = new Random();
             Point curPoint = new Point(0, MapHeight / 2);
             int tileCount = 0;
+            bool isPrevVertical = false;
+            bool spawnTwo = false;
+            int prevSide = 0;
+
 
             while (true)
             {
                 int side = rnd.Next(0, 101);
 
                 if (side < 60) // RIGHT 
+                {
+                    if(isPrevVertical)
+                    {
+                        curPoint = new Point(curPoint.X + 1, curPoint.Y);
+
+                        Tile sideControl = new Tile();
+                        sideControl.Size = new Size(TileSize, TileSize);
+                        sideControl.Location = new Point(curPoint.X * TileSize, curPoint.Y * TileSize);
+                        sideControl.Image = Image.FromFile("../../Sprites/roadUP.png");
+                        sideControl.Name = "Road" + tileCount;
+                        form.Controls.Add(sideControl);
+                        sideControl.BringToFront();
+
+                        tileCount++;
+
+                        if (curPoint.X == MapWidth - 1)
+                            break;
+                    }
                     curPoint = new Point(curPoint.X + 1, curPoint.Y);
+                    prevSide = 0;
+                    isPrevVertical = false;
+                }
+                    
 
                 else if (side < 80) //UP
                     if (curPoint.Y >= 0)
+                    {
+                        if(prevSide == 2)
+                        {
+                            continue;
+                        }
                         curPoint = new Point(curPoint.X, curPoint.Y + 1);
+                        isPrevVertical = true;
+                        prevSide = 1;
+                    }
+                        
                     else
                         return;
 
                 else if (side < 100)//DOWN
                     if (curPoint.Y <= MapHeight)
+                    {
+                        if(prevSide == 1)
+                        {
+                            continue;
+                        }
+
                         curPoint = new Point(curPoint.X, curPoint.Y - 1);
+                        isPrevVertical = true;
+                        prevSide = 2;
+                    }
+
                     else
                         return;
 
                 Tile control = new Tile();
                 control.Size = new Size(TileSize, TileSize);
                 control.Location = new Point(curPoint.X * TileSize, curPoint.Y * TileSize);
-                control.Image = Image.FromFile("../../resources/road.png");
+                control.Image = Image.FromFile("../../Sprites/roadUP.png");
                 control.Name = "Road" + tileCount;
                 form.Controls.Add(control);
                 control.BringToFront();
@@ -58,24 +103,32 @@ namespace Budget_game
             Tile firstTile = new Tile();
             firstTile.Size = new Size(TileSize, TileSize);
             firstTile.Location = new Point(0, MapHeight * TileSize / 2);
-            firstTile.Image = Image.FromFile("../../resources/road.png");
+            firstTile.Image = Image.FromFile("../../Sprites/roadUP.png");
             form.Controls.Add(firstTile);
             firstTile.BringToFront();
         }
 
         private static void GenerateMap(Form form)
         {
+            Random rnd = new Random();
             int tileCount = 0;
 
             for (int i = 0; i < MapWidth; i++)
             {
                 for (int j = 0; j < MapHeight; j++)
                 {
+
                     Tile control = new Tile();
                     control.Size = new Size(TileSize, TileSize);
                     control.Name = "Terrain" + tileCount;
                     control.Location = new Point(i * TileSize, j * TileSize);
-                    control.Image = Image.FromFile("../../resources/test.jpg");
+                    control.Image = Image.FromFile("../../Sprites/grass.png");
+
+                    int chance = rnd.Next(0, 101);
+                    if (chance < 70)
+                        control.Image = Image.FromFile("../../Sprites/grass.png");
+                    else
+                        control.Image = Image.FromFile("../../Sprites/water.png");
 
                     form.Controls.Add(control);
                     tileCount++;
