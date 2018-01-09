67a17d6c4f566bed07386e7a4e3d6723f19f24b5
hindrekhan <hindrek.hannus@tptlive.ee>
2018-01-09 12:45:34 +0200

    Revert "Merge branch 'UI'"
    This reverts commit 4445436a35f152365e9a988d858af7cf13ac1c0b, reversing
    changes made to e095bffcd0a920dd9ce7f1848098eb5271261e66.

--- Budget game/Budget game/Program.cs
+++ Budget game/Budget game/Program.cs
@@ -16,7 +16,7 @@ namespace Budget_game
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Game());
+            Application.Run(new Form1());
         }
     }
 }
