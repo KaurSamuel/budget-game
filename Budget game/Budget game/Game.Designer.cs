47aa79ef12a0d3386bf43f1586f3bd08b8ee0a5c
JaanJah <jaanmarkus.jahilo@tptlive.ee>
2018-01-09 09:52:56 +0200

    UI

---
+++ Budget game/Budget game/Game.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace Budget_game
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblHitpointsName = new System.Windows.Forms.Label();
+            this.pctMapTerrain = new System.Windows.Forms.PictureBox();
+            this.lblGoldName = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lblEnemiesLeftName = new System.Windows.Forms.Label();
+            this.lblHitpoints = new System.Windows.Forms.Label();
+            this.lblGold = new System.Windows.Forms.Label();
+            this.lblEnemiesLeft = new System.Windows.Forms.Label();
+            this.lblLevelName = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.lblShopName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.lblTower1Price = new System.Windows.Forms.Label();
+            this.lblTower2Price = new System.Windows.Forms.Label();
+            this.lblTower3Price = new System.Windows.Forms.Label();
+            this.lblTower4Price = new System.Windows.Forms.Label();
+            this.lblUpgradesName1 = new System.Windows.Forms.Label();
+            this.lblUpgradesName2 = new System.Windows.Forms.Label();
+            this.lblUpgradesName3 = new System.Windows.Forms.Label();
+            this.lblUpgradesName4 = new System.Windows.Forms.Label();
+            this.cmbUpgradesTower1 = new System.Windows.Forms.ComboBox();
+            this.cmbUpgradesTower2 = new System.Windows.Forms.ComboBox();
+            this.cmbUpgradesTower3 = new System.Windows.Forms.ComboBox();
+            this.cmbUpgradesTower4 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctMapTerrain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHitpointsName
+            // 
+            this.lblHitpointsName.AutoSize = true;
+            this.lblHitpointsName.Location = new System.Drawing.Point(1041, 24);
+            this.lblHitpointsName.Name = "lblHitpointsName";
+            this.lblHitpointsName.Size = new System.Drawing.Size(25, 13);
+            this.lblHitpointsName.TabIndex = 0;
+            this.lblHitpointsName.Text = "HP:";
+            // 
+            // pctMapTerrain
+            // 
+            this.pctMapTerrain.Location = new System.Drawing.Point(26, 24);
+            this.pctMapTerrain.Name = "pctMapTerrain";
+            this.pctMapTerrain.Size = new System.Drawing.Size(1000, 500);
+            this.pctMapTerrain.TabIndex = 1;
+            this.pctMapTerrain.TabStop = false;
+            // 
+            // lblGoldName
+            // 
+            this.lblGoldName.AutoSize = true;
+            this.lblGoldName.Location = new System.Drawing.Point(1041, 49);
+            this.lblGoldName.Name = "lblGoldName";
+            this.lblGoldName.Size = new System.Drawing.Size(32, 13);
+            this.lblGoldName.TabIndex = 2;
+            this.lblGoldName.Text = "Gold:";
+            // 
+            // lblEnemiesLeftName
+            // 
+            this.lblEnemiesLeftName.AutoSize = true;
+            this.lblEnemiesLeftName.Location = new System.Drawing.Point(1041, 74);
+            this.lblEnemiesLeftName.Name = "lblEnemiesLeftName";
+            this.lblEnemiesLeftName.Size = new System.Drawing.Size(67, 13);
+            this.lblEnemiesLeftName.TabIndex = 3;
+            this.lblEnemiesLeftName.Text = "Enemies left:";
+            // 
+            // lblHitpoints
+            // 
+            this.lblHitpoints.AutoSize = true;
+            this.lblHitpoints.Location = new System.Drawing.Point(1116, 24);
+            this.lblHitpoints.Name = "lblHitpoints";
+            this.lblHitpoints.Size = new System.Drawing.Size(156, 13);
+            this.lblHitpoints.TabIndex = 4;
+            this.lblHitpoints.Text = "only here to find the label easier";
+            // 
+            // lblGold
+            // 
+            this.lblGold.AutoSize = true;
+            this.lblGold.Location = new System.Drawing.Point(1116, 49);
+            this.lblGold.Name = "lblGold";
+            this.lblGold.Size = new System.Drawing.Size(156, 13);
+            this.lblGold.TabIndex = 5;
+            this.lblGold.Text = "only here to find the label easier";
+            // 
+            // lblEnemiesLeft
+            // 
+            this.lblEnemiesLeft.AutoSize = true;
+            this.lblEnemiesLeft.Location = new System.Drawing.Point(1114, 74);
+            this.lblEnemiesLeft.Name = "lblEnemiesLeft";
+            this.lblEnemiesLeft.Size = new System.Drawing.Size(156, 13);
+            this.lblEnemiesLeft.TabIndex = 6;
+            this.lblEnemiesLeft.Text = "only here to find the label easier";
+            // 
+            // lblLevelName
+            // 
+            this.lblLevelName.AutoSize = true;
+            this.lblLevelName.Location = new System.Drawing.Point(1041, 96);
+            this.lblLevelName.Name = "lblLevelName";
+            this.lblLevelName.Size = new System.Drawing.Size(36, 13);
+            this.lblLevelName.TabIndex = 7;
+            this.lblLevelName.Text = "Level:";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Location = new System.Drawing.Point(1114, 96);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(156, 13);
+            this.lblLevel.TabIndex = 8;
+            this.lblLevel.Text = "only here to find the label easier";
+            // 
+            // lblShopName
+            // 
+            this.lblShopName.AutoSize = true;
+            this.lblShopName.Location = new System.Drawing.Point(23, 527);
+            this.lblShopName.Name = "lblShopName";
+            this.lblShopName.Size = new System.Drawing.Size(35, 13);
+            this.lblShopName.TabIndex = 9;
+            this.lblShopName.Text = "Shop:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(64, 527);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(278, 527);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(491, 527);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(697, 527);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // lblTower1Price
+            // 
+            this.lblTower1Price.AutoSize = true;
+            this.lblTower1Price.Location = new System.Drawing.Point(61, 562);
+            this.lblTower1Price.Name = "lblTower1Price";
+            this.lblTower1Price.Size = new System.Drawing.Size(75, 13);
+            this.lblTower1Price.TabIndex = 14;
+            this.lblTower1Price.Text = "add price here";
+            // 
+            // lblTower2Price
+            // 
+            this.lblTower2Price.AutoSize = true;
+            this.lblTower2Price.Location = new System.Drawing.Point(275, 562);
+            this.lblTower2Price.Name = "lblTower2Price";
+            this.lblTower2Price.Size = new System.Drawing.Size(75, 13);
+            this.lblTower2Price.TabIndex = 15;
+            this.lblTower2Price.Text = "add price here";
+            // 
+            // lblTower3Price
+            // 
+            this.lblTower3Price.AutoSize = true;
+            this.lblTower3Price.Location = new System.Drawing.Point(488, 562);
+            this.lblTower3Price.Name = "lblTower3Price";
+            this.lblTower3Price.Size = new System.Drawing.Size(75, 13);
+            this.lblTower3Price.TabIndex = 16;
+            this.lblTower3Price.Text = "add price here";
+            // 
+            // lblTower4Price
+            // 
+            this.lblTower4Price.AutoSize = true;
+            this.lblTower4Price.Location = new System.Drawing.Point(694, 562);
+            this.lblTower4Price.Name = "lblTower4Price";
+            this.lblTower4Price.Size = new System.Drawing.Size(75, 13);
+            this.lblTower4Price.TabIndex = 17;
+            this.lblTower4Price.Text = "add price here";
+            // 
+            // lblUpgradesName1
+            // 
+            this.lblUpgradesName1.AutoSize = true;
+            this.lblUpgradesName1.Location = new System.Drawing.Point(61, 591);
+            this.lblUpgradesName1.Name = "lblUpgradesName1";
+            this.lblUpgradesName1.Size = new System.Drawing.Size(56, 13);
+            this.lblUpgradesName1.TabIndex = 18;
+            this.lblUpgradesName1.Text = "Upgrades:";
+            // 
+            // lblUpgradesName2
+            // 
+            this.lblUpgradesName2.AutoSize = true;
+            this.lblUpgradesName2.Location = new System.Drawing.Point(294, 591);
+            this.lblUpgradesName2.Name = "lblUpgradesName2";
+            this.lblUpgradesName2.Size = new System.Drawing.Size(56, 13);
+            this.lblUpgradesName2.TabIndex = 19;
+            this.lblUpgradesName2.Text = "Upgrades:";
+            // 
+            // lblUpgradesName3
+            // 
+            this.lblUpgradesName3.AutoSize = true;
+            this.lblUpgradesName3.Location = new System.Drawing.Point(497, 591);
+            this.lblUpgradesName3.Name = "lblUpgradesName3";
+            this.lblUpgradesName3.Size = new System.Drawing.Size(56, 13);
+            this.lblUpgradesName3.TabIndex = 20;
+            this.lblUpgradesName3.Text = "Upgrades:";
+            // 
+            // lblUpgradesName4
+            // 
+            this.lblUpgradesName4.AutoSize = true;
+            this.lblUpgradesName4.Location = new System.Drawing.Point(703, 591);
+            this.lblUpgradesName4.Name = "lblUpgradesName4";
+            this.lblUpgradesName4.Size = new System.Drawing.Size(56, 13);
+            this.lblUpgradesName4.TabIndex = 21;
+            this.lblUpgradesName4.Text = "Upgrades:";
+            // 
+            // cmbUpgradesTower1
+            // 
+            this.cmbUpgradesTower1.FormattingEnabled = true;
+            this.cmbUpgradesTower1.Location = new System.Drawing.Point(54, 607);
+            this.cmbUpgradesTower1.Name = "cmbUpgradesTower1";
+            this.cmbUpgradesTower1.Size = new System.Drawing.Size(82, 21);
+            this.cmbUpgradesTower1.TabIndex = 22;
+            // 
+            // cmbUpgradesTower2
+            // 
+            this.cmbUpgradesTower2.FormattingEnabled = true;
+            this.cmbUpgradesTower2.Location = new System.Drawing.Point(278, 607);
+            this.cmbUpgradesTower2.Name = "cmbUpgradesTower2";
+            this.cmbUpgradesTower2.Size = new System.Drawing.Size(94, 21);
+            this.cmbUpgradesTower2.TabIndex = 23;
+            // 
+            // cmbUpgradesTower3
+            // 
+            this.cmbUpgradesTower3.FormattingEnabled = true;
+            this.cmbUpgradesTower3.Location = new System.Drawing.Point(491, 607);
+            this.cmbUpgradesTower3.Name = "cmbUpgradesTower3";
+            this.cmbUpgradesTower3.Size = new System.Drawing.Size(72, 21);
+            this.cmbUpgradesTower3.TabIndex = 24;
+            // 
+            // cmbUpgradesTower4
+            // 
+            this.cmbUpgradesTower4.FormattingEnabled = true;
+            this.cmbUpgradesTower4.Location = new System.Drawing.Point(697, 607);
+            this.cmbUpgradesTower4.Name = "cmbUpgradesTower4";
+            this.cmbUpgradesTower4.Size = new System.Drawing.Size(72, 21);
+            this.cmbUpgradesTower4.TabIndex = 25;
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.cmbUpgradesTower4);
+            this.Controls.Add(this.cmbUpgradesTower3);
+            this.Controls.Add(this.cmbUpgradesTower2);
+            this.Controls.Add(this.cmbUpgradesTower1);
+            this.Controls.Add(this.lblUpgradesName4);
+            this.Controls.Add(this.lblUpgradesName3);
+            this.Controls.Add(this.lblUpgradesName2);
+            this.Controls.Add(this.lblUpgradesName1);
+            this.Controls.Add(this.lblTower4Price);
+            this.Controls.Add(this.lblTower3Price);
+            this.Controls.Add(this.lblTower2Price);
+            this.Controls.Add(this.lblTower1Price);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblShopName);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.lblLevelName);
+            this.Controls.Add(this.lblEnemiesLeft);
+            this.Controls.Add(this.lblGold);
+            this.Controls.Add(this.lblHitpoints);
+            this.Controls.Add(this.lblEnemiesLeftName);
+            this.Controls.Add(this.lblGoldName);
+            this.Controls.Add(this.pctMapTerrain);
+            this.Controls.Add(this.lblHitpointsName);
+            this.Name = "Game";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pctMapTerrain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHitpointsName;
+        private System.Windows.Forms.PictureBox pctMapTerrain;
+        private System.Windows.Forms.Label lblGoldName;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lblEnemiesLeftName;
+        private System.Windows.Forms.Label lblHitpoints;
+        private System.Windows.Forms.Label lblGold;
+        private System.Windows.Forms.Label lblEnemiesLeft;
+        private System.Windows.Forms.Label lblLevelName;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Label lblShopName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lblTower1Price;
+        private System.Windows.Forms.Label lblTower2Price;
+        private System.Windows.Forms.Label lblTower3Price;
+        private System.Windows.Forms.Label lblTower4Price;
+        private System.Windows.Forms.Label lblUpgradesName1;
+        private System.Windows.Forms.Label lblUpgradesName2;
+        private System.Windows.Forms.Label lblUpgradesName3;
+        private System.Windows.Forms.Label lblUpgradesName4;
+        private System.Windows.Forms.ComboBox cmbUpgradesTower1;
+        private System.Windows.Forms.ComboBox cmbUpgradesTower2;
+        private System.Windows.Forms.ComboBox cmbUpgradesTower3;
+        private System.Windows.Forms.ComboBox cmbUpgradesTower4;
+    }
+}
+
