7db53c77ee9f8e0a3cc223971f6905388f278346
JaanJah <jaanmarkus.jahilo@tptlive.ee>
2018-01-09 10:34:25 +0200

    monster ja player classid

---
+++ Budget game/Budget game/Monster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget_game
+{
+    class Monster
+    {
+        public int MaximumHealth { get; set; }
+        public int CurrentHealth { get; set; }
+        public int Gold { get; set; }
+    }
+}
---
+++ Budget game/Budget game/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget_game
+{
+    class Player
+    {
+        public float PlayerLives = 10;
+        public float PlayerLivesLeft { get; set; }
+        public int Gold { get; set; }
+        public int Level { get; set; }
+    }
+}
