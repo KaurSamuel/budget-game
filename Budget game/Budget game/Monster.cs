293581f7d67a698131cc7972ad9beab6d2430dd4
hindrekhan <hindrek.hannus@tptlive.ee>
2018-01-11 13:15:56 +0200

    fixed coins

--- Budget game/Budget game/Monster.cs
+++ Budget game/Budget game/Monster.cs
@@ -62,7 +62,7 @@ namespace Budget_game
         {
             CurrentHealth -= damage;
 
-            if (CurrentHealth <= 0)
+            if (CurrentHealth <= 0 && IsAlive)
             {
                 IsAlive = false;
                 form.Controls.Remove(this);
